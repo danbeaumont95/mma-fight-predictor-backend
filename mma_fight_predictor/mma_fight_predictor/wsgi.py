9a023bf933f9b64bde245ac8b6d7b53c6dc677aa
Dan Beaumont <danielbeaumont95@hotmail.co.uk>
2023-08-19 22:08:46 +0100

    render hosting

--- mma_fight_predictor/mma_fight_predictor/wsgi.py
+++ mma_fight_predictor/mma_fight_predictor/wsgi.py
@@ -12,7 +12,7 @@ import sys
 from django.core.wsgi import get_wsgi_application
 sys.path.append('/mma_fight_predictor')
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mma_fight_predictor.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mma_fight_predictor.mma_fight_predictor.settings')
 
 
 application = get_wsgi_application()
