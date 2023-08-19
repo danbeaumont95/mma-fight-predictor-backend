123f3e8485dde20ee84b9f8255267ee99b1af548
Dan Beaumont <danielbeaumont95@hotmail.co.uk>
2023-08-19 21:34:43 +0100

    render hosting

--- mma_fight_predictor/mma_fight_predictor/settings.py
+++ mma_fight_predictor/mma_fight_predictor/settings.py
@@ -42,7 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    # 'mma_fight_predictor_api',
+    'mma_fight_predictor_api',
     'mma_fight_predictor_api123',
     'django_extensions',
     'corsheaders',
