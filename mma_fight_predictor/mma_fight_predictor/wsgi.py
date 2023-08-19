5938e7313fb197937dfc3aa1a37fa219ad026cc7
Dan Beaumont <danielbeaumont95@hotmail.co.uk>
2023-08-19 20:27:17 +0100

    changed allowd hosts

--- mma_fight_predictor/mma_fight_predictor/wsgi.py
+++ mma_fight_predictor/mma_fight_predictor/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mma_fight_predictor.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mma_fight_predictor.mma_fight_predictor.settings')
 
 application = get_wsgi_application()
