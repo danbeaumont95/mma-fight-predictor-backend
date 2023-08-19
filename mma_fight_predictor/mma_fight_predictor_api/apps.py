441056da02d1768a9195db7e848103c026bde137
Dan Beaumont <danielbeaumont95@hotmail.co.uk>
2023-08-19 21:42:19 +0100

    render hosting

--- mma_fight_predictor/mma_fight_predictor_api/apps.py
+++ mma_fight_predictor/mma_fight_predictor_api/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class MmaFightPredictorApiConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'mma_fight_predictor_api'
+    name = 'mma_fight_predictor.mma_fight_predictor_api'
